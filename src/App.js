760aa35cfa7d9fb4e58a07cf7c4505116c7b9bca
ReymerTriana <reymertriana@gmail.com>
2025-02-21 02:52:31 -0500

    PROYECTO INICIAL REACT 17 FUNCIONANDO

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [data, setData] = useState("Anil");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hooks in React {data}</h1>
+      <button onClick={() => setData("Peter")}>Update State</button>
     </div>
   );
 }
